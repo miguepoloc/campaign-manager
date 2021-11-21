a223812bd9e2fcad5c563abfb64584fbd4792d4c
miguepoloc <miguepoloc@gmail.com>
2021-11-21 17:04:04 -0500

    Dandole estilos al frontend

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,11 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React, { useState, useEffect } from 'react';
+import dateformat from "dateformat"
 
 export default function Home({ data, error }) {
-
-  console.log("Data: >>", data);
-  console.log("Error: >>", error);
 
   useEffect(() => {
     console.log(`process.env.NEXT_PUBLIC_BASE_URL`, process.env.NEXT_PUBLIC_BASE_URL)
@@ -14,23 +12,34 @@ export default function Home({ data, error }) {
 
   return (
     <div>
-      <main>
-        <h1>Campañas disponibles</h1>
-        {error && <p>{JSON.stringify(error)}</p>}
-        {data.map((element) => <div key={element.slug}>
-          <div>
-            <div>
-              <Image src={"https://res.cloudinary.com/djw2ks8ek/" + element.logo} height={120} width={120} alt="Campaña banner" />
-            </div>
-            <div>
-              <h1>{element.title}</h1>
-              <p>{element.description} </p>
-              <p>{element.created_at} </p>
-            </div>
-          </div>
 
-        </div>)}
-        <p> Nueva fuente</p>
+      <Head>
+        <title>Administrador de campaña: Home</title>
+        <meta name="description" content="Un sitio de campañas"></meta>
+      </Head>
+
+      <main className={styles.main}>
+
+        <div className={styles.innerContent}>
+          <h1>Campañas disponibles</h1>
+          {error && <p>{JSON.stringify(error)}</p>}
+          {data.map((element) => <div key={element.slug}>
+
+            <div className={styles.item}>
+              <div className={styles.imgContainer}>
+                <Image className={styles.img} src={"https://res.cloudinary.com/djw2ks8ek/" + element.logo} height={120} width={120} alt="Campaña banner" />
+              </div>
+
+              <div className={styles.rightItems}>
+                <h1>{element.title}</h1>
+                <p>{element.description} </p>
+                <small>{dateformat(new Date(element.created_at), "dddd, mmmm, dS, yyyy, h:MM:ss TT")} </small>
+              </div>
+            </div>
+
+          </div>)}
+        </div>
+
       </main>
 
     </div>
